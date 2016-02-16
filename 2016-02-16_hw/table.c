e1f025ddd213eabeba4d97798000759ffbb8610d
Abdullah Khan <khanabdullahj@gmail.com>
2016-02-16 00:23:40 -0500

    Finished table.c

--- 2016-02-16_hw/table.c
+++ 2016-02-16_hw/table.c
@@ -7,17 +7,9 @@
  * Build instructions: make
  */
 
-/*
- * <pwd.h> header provides a definition for struct passwd,
- * which includes members like uid_t pw_uid, numerical user ID
- * and gid_t pw_gid, numerical group ID.
-*/
 #include <stdio.h> /* For standard input and output functions. */
 #include <stdlib.h> /* For exit() */
-#include <unistd.h> /* POSIX API */
-#include <fcntl.h> /* Definition of AT_* constants */
 #include <sys/stat.h> /* For mode_t */
-#include <sys/types.h> /* For any POSIX specific types. */
 
 int main(int argc, char ** argv)
 {
@@ -35,23 +27,23 @@ int main(int argc, char ** argv)
 
     printf("File status for: %s\n", argv[1]);
     printf("---------------------------\n");
-    printf("Size: \t\t\t%llu bytes\n",file_status.st_size);
-    printf("# of Links: \t\t\t%llu\n",file_status.st_nlink);
-    printf("File inode: \t\t%llu\n",file_status.st_ino);
+    printf("Size: \t\t\t%lu bytes\n",file_status.st_size);
+    printf("# of Links: \t\t\t%lu\n",file_status.st_nlink);
+    printf("File inode: \t\t%lu\n",file_status.st_ino);
 
     printf("File Permissions: \t");
     printf((S_ISDIR(file_status.st_mode)) ? "1" : "0");
 
     printf((file_status.st_mode & S_ISUID) ? "1" : "0");
     printf((file_status.st_mode & S_ISGID) ? "1" : "0");
-    printf((file_status.st_mode & S_ISVTX) ? "1" : "0");
+    //printf((file_status.st_mode & S_ISVTX) ? "1" : "0");
 
-    printf((file_status.st_mode & S_ISWXU) ? "1" : "0");
+    //printf((file_status.st_mode & S_ISWXU) ? "1" : "0");
     printf((file_status.st_mode & S_IRUSR) ? "1" : "0");
     printf((file_status.st_mode & S_IWUSR) ? "1" : "0");
     printf((file_status.st_mode & S_IXUSR) ? "1" : "0");
 
-    printf((file_status.st_mode & S_ISWXG) ? "1" : "0");
+    //printf((file_status.st_mode & S_ISWXG) ? "1" : "0");
     printf((file_status.st_mode & S_IRGRP) ? "1" : "0");
     printf((file_status.st_mode & S_IWGRP) ? "1" : "0");
     printf((file_status.st_mode & S_IXGRP) ? "1" : "0");
@@ -61,7 +53,7 @@ int main(int argc, char ** argv)
     printf((file_status.st_mode & S_IWOTH) ? "1" : "0");
     printf((file_status.st_mode & S_IXOTH) ? "1" : "0");
 
-    printf("\n\n");
+    printf("\n");
 
-    printf("Symbolic link: %s\n", (S_ISLNK(file_status.st_mode)) ? "yes" : "no");
+    exit(0);
 }
