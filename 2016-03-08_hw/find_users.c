ae5c013c03ea28fd1f290f53b0e4e50ac91a8f73
Abdullah Khan <khanabdullahj@gmail.com>
2016-03-15 15:07:42 -0400

    2016-03-08_hw part 1 complete.

---
+++ 2016-03-08_hw/find_users.c
@@ -0,0 +1,40 @@
+/*
+ * Author: Abdullah Khan
+ * Date: 2016-03-01
+ * Program: maketemp.c
+ * Description: Creates a temporary file called /tmp/tempFILE and creates a table
+ * with the time of execution in it.
+ * Build instructions: make
+ */
+
+/* If using a compiler that defaults to C99 or later */
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 700
+#else
+#define _XOPEN_SOURCE 600
+#endif /* __STDC_VERSION__ */
+
+// STDC includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// UNIX includes
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
+
+int main(int argc, char ** argv)
+{
+	struct passwd *pass;
+	setpwent(); // Takes us to beginning of password database.
+	while((pass = getpwent()) != NULL) // Iterate through /etc/passwd
+	{
+		printf("User: %20s\t UID: %6u\t\t GID: %6u\n",
+			pass->pw_name,
+			pass->pw_uid,
+			pass->pw_gid);
+	}
+	printf("\n");
+	exit(EXIT_SUCCESS);
+}
