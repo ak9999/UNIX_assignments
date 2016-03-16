24dcdb2476888698666d2f65d0e3916f2b05f6da
Abdullah Khan <khanabdullahj@gmail.com>
2016-03-15 20:39:20 -0400

    finished

--- 2016-03-08_hw/group_names.c
+++ 2016-03-08_hw/group_names.c
@@ -1,10 +1,10 @@
 /*
- * Author: Abdullah Khan
- * Date: 2016-03-01
- * Program: group_names.c
- * Description: Prints out groups a user belongs to.
- * Build instructions: make
- */
+Author: Abdullah Khan
+Date: 2016-03-01
+Program: group_names.c
+Description: Prints out groups a user belongs to.
+Build instructions: make
+*/
 
 /* If using a compiler that defaults to C99 or later */
 #if __STDC_VERSION__ >= 199901L
@@ -13,32 +13,52 @@
 #define _XOPEN_SOURCE 600
 #endif /* __STDC_VERSION__ */
 
-// STDC includes
+// C includes
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 // UNIX includes
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/param.h> // for NGROUPS_MAX
+#include <pwd.h>
 #include <grp.h>
 
-int main(int argc, char ** argv)
+int main(int argc, char * argv[])
 {
-	struct group *ptr;
-	setgrent(); // Open the /etc/group at the beginning.
-	gid_t grouplist[NGROUPS_MAX];
-	int num_groups = getgroups(NGROUPS_MAX, grouplist);
-	for(int i = 0; i < num_groups; i++)
+	int numGroups;
+	gid_t *grouplist;
+	struct group *gr;
+
+	grouplist = (gid_t*)malloc(sizeof(gid_t) * NGROUPS_MAX);
+
+	// Get number of groups the user is in.
+	if((numGroups = getgroups(0,NULL)) < 0)
 	{
-		gid_t curr_id = grouplist[i];
-		if(getgrgid(curr_id))
+		printf("ERROR: getgroups() failure #1.");
+		exit(1);
+	}
+
+	if((numGroups = getgroups(NGROUPS_MAX, grouplist)) < 0)
+	{
+		printf("ERROR: getgroups() failure #2.");
+		exit(2);
+	}
+
+	if(numGroups >= 0)
+	{
+		// Print out GIDs and group names.
+		printf("GIDs\t%20s\n", "Group names");
+		for(unsigned int i = 0; i < numGroups; i++)
 		{
-			printf("GID: %6u\t Group name: %20s\n",
-				   	grouplist[i],
-				   	ptr->gr_name);
+			printf("%u\t", grouplist[i]);
+			if((gr = getgrgid(grouplist[i])) != NULL)
+			{
+				printf("%20s\n", gr->gr_name);
+			}
 		}
 	}
-	endgrent(); // Close /etc/groups
+	printf("\n");
 	exit(EXIT_SUCCESS);
 }
